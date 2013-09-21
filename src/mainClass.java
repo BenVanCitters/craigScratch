b17b859eb9f13d6920a8b2698b73f2fa904d919b
Ben Van CItters <van.citters.b@gmail.com>
2013-09-20 19:09:39 -0700

    a little cleanup

--- src/mainClass.java
+++ src/mainClass.java
@@ -18,38 +18,61 @@ public class mainClass {
         {
         	doc = Jsoup.connect(url).get();        	
         }
-        catch(java.lang.IllegalArgumentException e)
-        {
+        catch(Exception e)
+        {        	
         	System.out.println(e);
+        	System.out.println("exception caught! - opening from disk");	
+        	File input = new File ("/Users/admin/Documents/workspace/craigScratch/craigslist page/site.html");
+        	doc = Jsoup.parse(input, "UTF-8", "");
         }
-//        finally
-//        {
-//        	System.out.println("finally");
-//        	File input = new File(url);
-//        	doc = Jsoup.parse(input, "UTF-8", null);	
-//        }
+
+        
+        Elements nxtSpans = doc.select("span.nplink");
+        for (Element nxtSpan : nxtSpans) {
+        	Elements nxtLinks = nxtSpan.select("a[href]");
+        	for (Element nxLnk : nxtLinks) {
+        		print(" * <%s>",nxLnk.attr("abs:href"));
+        	}
+        }
+        
         Elements links = doc.select("p.row");
-/*
- * <p class="row" data-latitude="47.675818130271" 
- * data-longitude="-122.398223569797" 
- * data-pid="4051365390"> <a href="/see/apa/4051365390.html" class="i"></a>
- *  <span class="pl"> <span class="star v" title="save this post in your favorites list"></span> <span class="date">Sep  6</span>  <a href="/see/apa/4051365390.html">Ballard Craftsman 3BR 1.5 Bath</a> </span> <span class="l2"> 
- *   <span class="price">$2400</span> / 3br -  <span class="pnr"> <small> (3047 NW 65th. St.)</small> <span class="px"> <span class="p"> <a href="#" class="maptag" data-pid="4051365390">map</a></span></span> </span>  </span> </p>
- */
+
+/* <p class="row" data-latitude="47.675818130271" data-longitude="-122.398223569797" data-pid="4051365390"> 
+ *   <a href="/see/apa/4051365390.html" class="i"></a>
+ *   <span class="pl"> 
+ *      <span class="star v" title="save this post in your favorites list"></span> 
+ *      <span class="date">Sep  6</span> 
+ *      <a href="/see/apa/4051365390.html">Ballard Craftsman 3BR 1.5 Bath</a> 
+ *   </span> 
+ *  <span class="l2"> 
+ *    <span class="price">$2400</span> / 3br -  
+ *      <span class="pnr"> 
+ *        <small> (3047 NW 65th. St.)</small> 
+ *        <span class="px"> 
+ *          <span class="p"> 
+ *          <a href="#" class="maptag" data-pid="4051365390">map</a>
+ *        </span>
+ *      </span> 
+ *    </span>  
+ *  </span> 
+ *  </p>
+ */        
         print("\nLinks: (%d)", links.size());
         for (Element link : links) {
         	float lat =0;
         	float lng = 0;
         	float cost = 0;
-        	String txt = link.attr("data-latitude");
-        	if(txt != "")
+        	String pid = link.attr("data-pid");
+        	String lattxt = link.attr("data-latitude");
+        	String lngtxt = link.attr("data-longitude");
+        	if((lattxt != "")&&(lngtxt != ""))
         	{
-        		lat = Float.parseFloat(txt);
+        		lat = Float.parseFloat(lattxt);
+        		lng = Float.parseFloat(lngtxt);
         	}
-        	txt = link.attr("data-longitude");
-        	if(txt != "")
+        	else
         	{
-        		lng = Float.parseFloat(txt);
+        		//continue;
         	}
             
             Elements price = link.select("span.price");
@@ -60,7 +83,8 @@ public class mainClass {
             	//print("txt: %f",cost);
 //            	link.attr("data-latitude");
             }
-            print(" * lat,long: %f, %f price $%f", lat,lng,cost);
+            
+            print(" *%s lat,long: %f, %f price $%.2f", pid,lat,lng,cost);
         }
     }
 
